6011f7b2a07e7c31dca69fa4239a06de86346975
Arricc <mark@arricc.net>
2018-08-30 12:43:04 +0100

    GEt-DriveSpace - updated logic for prompting for credentials to only prompt once

--- Functions/Get-DriveSpace.ps1
+++ Functions/Get-DriveSpace.ps1
@@ -2,26 +2,35 @@
     [cmdletbinding()]
     param( 
         [parameter(Position=1,ValueFromPipeline)]
-        [string]$Computer = $env:COMPUTERNAME
+        [string]$Computer = $env:COMPUTERNAME,
+        [string]$DomainName
     )
+    Begin {
+        $cred=$null 
+
+    }
 
     Process {
-
+        if ($DomainName -and $Computer -notlike "*.*") {$Computer = "$Computer.$DomainName"} 
+        
         if ($computer -like "*.*") {
-            $cred = Get-Credential
+            if(!$cred)  { $cred = Get-Credential }
             $qry = Get-WmiObject Win32_LogicalDisk -Filter drivetype=3 -ComputerName $computer -Credential $cred 
         }
         else { 
             $qry = Get-WmiObject Win32_LogicalDisk -filter drivetype=3 -comp $computer 
         }  
         $qry | % {
-            new-object psobject -property @{ 
+            $hash = [ordered]@{ 
                 Drive   = $_.deviceID; 
                 GBFree  = "{0:N2}" -f ($_.freespace / 1gb) ;
                 TotalGB = "{0:N0}" -f ($_.size / 1gb) ;
                 FreePct = "{0:P0}" -f ($_.FreeSpace / $_.size);
                 Name    = $_.volumeName
+                Computer = $Computer
             }
+            new-object psobject -property $hash 
+            
         }
     }
 }
