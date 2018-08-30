08d59f4ebe231e1a8ae94e7a1cfca8900a572685
Arricc <mark@arricc.net>
2018-08-30 17:10:32 +0100

    README - added initial install instructions Install-PSProfile - not

--- Functions/Install-PSProfile.ps1
+++ Functions/Install-PSProfile.ps1
@@ -1,20 +1,14 @@
-# PSProfile
-My PowerShell Profile (Minus some local extras I use on my work machine).
+##
+## Install or update the profile
+##
 
-A chunk of this is "borrowed" from :
-* [@KennyLowe](https://twitter.com/KennyLowe) - [GitHub Project](https://github.com/KennyLowe/PProfile)
-* [u/PowerShellStunnah](https://www.reddit.com/user/PowerShellStunnah) - version of [Get-Excuse](https://www.reddit.com/r/PowerShell/comments/2x8n3y/getexcuse/coz53xa/)
-* @Others TBC.
-
-# Install/Update
-~~~~
 $path = ($env:TEMP + "\PSProfile")`
 $dest = $env:USERPROFILE + "\Documents\WindowsPowerShell"`
 $URL = "https://github.com/arricc/PSProfile/archive/master.zip" 
 
 
 md $path 
-Remove-Item $dest -Force
+Remove-Item $dest\* -Force -
 md $dest
 
 $pieces = $url.Split("/")
@@ -32,4 +26,6 @@ $destination.Copyhere($shellApp.namespace($unzipped).items())
  
 Copy-Item ($path + "\PSProfile-master\*") $env:USERPROFILE -recurse
  
-Remove-Item $path -recurse -force 
+Get-ChildItem -Path  $dest  -exclude "Transcripts" | foreach ($_) {
+    Remove-Item $_.fullname -Force -Recurse
+}
